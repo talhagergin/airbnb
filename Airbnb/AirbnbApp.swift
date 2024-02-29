0fbfea9c3bf7efe1b9f993740bc59230546b28b0
Talha Gergin <talhagergin6@gmail.com>
2024-02-29 18:52:50 +0300

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Talha Gergin on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
